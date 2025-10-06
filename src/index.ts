3bc797bb159a04f9de31460ef4623a9f62a84661
Ben Durrant <ben.durrant@marketdojo.com>
2025-10-06 17:05:26 +0100

    try implementation with fewer listeners

--- src/index.ts
+++ src/index.ts
@@ -2,58 +2,75 @@ import type { Types } from "./constants";
 import { types } from "./constants";
 import type { EventTargetLike, EventForType, EventTypes } from "./types";
 
-async function* onImpl<T extends EventTargetLike, E extends EventTypes<T>>(
+interface OnOptions extends AddEventListenerOptions {
+  /**
+   * Whether to throw an error or complete when the signal (if provided) is aborted.
+   *
+   * @default true
+   */
+  throwOnAbort?: boolean;
+}
+
+function onImpl<T extends EventTargetLike, E extends EventTypes<T>>(
   target: T,
   type: E,
-  opts: Omit<AddEventListenerOptions, "once"> = {},
-): AsyncGenerator<EventForType<T, E>> {
-  const { signal } = opts;
-  let reason: unknown;
-  let aborted = false as boolean;
-  if (signal) {
-    signal.addEventListener("abort", () => {
-      aborted = true;
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      ({ reason } = signal);
-    });
-  }
-  while (!aborted) {
-    yield new Promise<EventForType<T, E>>((resolve, reject) => {
-      const ac = new AbortController();
-      function onAbort() {
-        reject(new Error("Iterator aborted", { cause: reason }));
-      }
-      if (aborted) {
-        // already aborted, so reject immediately - don't listen for events
-        onAbort();
-        return;
-      }
-      // reject the promise if the listener gets removed
-      signal?.addEventListener("abort", onAbort, { signal: ac.signal });
+  { signal: parentSignal, throwOnAbort = true, ...opts }: OnOptions = {},
+): AsyncIterableIterator<EventForType<T, E>> {
+  let current = Promise.withResolvers<IteratorResult<EventForType<T, E>>>();
 
-      target.addEventListener(
-        type,
-        (ev) => {
-          // listener got called, we can stop listening for abort
-          ac.abort();
+  const returnAc = new AbortController();
 
-          resolve(ev as EventForType<T, E>);
-        },
-        {
-          once: true,
-          ...opts,
-        },
+  function onAbort() {
+    if (throwOnAbort) {
+      current.reject(
+        new Error("Iterator aborted", { cause: parentSignal?.reason }),
       );
-    });
+    } else {
+      current.resolve({ done: true, value: undefined });
+    }
   }
+
+  parentSignal?.addEventListener("abort", onAbort);
+
+  if (parentSignal?.aborted) {
+    onAbort();
+  } else {
+    target.addEventListener(
+      type,
+      (ev) => {
+        current.resolve({ done: false, value: ev as EventForType<T, E> });
+        current = Promise.withResolvers();
+      },
+      {
+        ...opts,
+        signal: parentSignal
+          ? AbortSignal.any([returnAc.signal, parentSignal])
+          : returnAc.signal,
+      },
+    );
+  }
+
+  return {
+    [Symbol.asyncIterator]() {
+      return this;
+    },
+    next() {
+      return current.promise;
+    },
+    return() {
+      returnAc.abort();
+      current.resolve({ done: true, value: undefined });
+      return current.promise;
+    },
+  };
 }
 
 const factory =
   <E extends string>(type: E) =>
   <T extends EventTargetLike>(
     target: T,
-    opts?: Omit<AddEventListenerOptions, "once">,
-  ): AsyncIterable<EventForType<T, E>> =>
+    opts?: OnOptions,
+  ): AsyncIterableIterator<EventForType<T, E>> =>
     onImpl(target, type as never, opts) as never;
 
 type EventFactories = {
