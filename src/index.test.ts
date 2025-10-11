c8c9021faaee8415d167daf80d58c7fcadd4c3bb
Ben Durrant <ben.durrant@marketdojo.com>
2025-10-12 00:26:53 +0100

    simplify tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,51 +2,45 @@ import { page } from "@vitest/browser/context";
 import { describe, expect, it, test } from "vitest";
 import { on } from ".";
 
-describe("on", () => {
-  const body = page.elementLocator(document.body);
-
-  async function clickThrice() {
-    for (let i = 0; i < 3; i++) {
-      await body.click();
-    }
+async function* take<T>(iter: AsyncIterable<T>, count: number) {
+  for await (const value of iter) {
+    yield value;
+    if (--count === 0) return;
   }
+}
 
+const body = page.elementLocator(document.body);
+
+async function clickThrice() {
+  for (let i = 0; i < 3; i++) {
+    await body.click();
+  }
+}
+
+describe("on", () => {
   it("creates an iterable of events", async () => {
     // could be Array.fromAsync(on.click(document).take(3)) one day
     // or with observables, document.on("click").take(3).toArray()
-    async function listen() {
-      const clickEvents: Array<PointerEvent> = [];
-      for await (const event of on.click(document)) {
-        clickEvents.push(event);
-        if (clickEvents.length === 3) {
-          break;
-        }
-      }
-      return clickEvents;
-    }
-
-    const listenPromise = listen();
+    const listenPromise = Array.fromAsync(take(on.click(document), 3));
 
     await clickThrice();
 
-    await expect(listenPromise).resolves.toHaveLength(3);
+    const events = await listenPromise;
+    expect(events).toHaveLength(3);
+    expect(events.every((event) => event instanceof PointerEvent)).toBe(true);
   });
   it("can be aborted", async () => {
-    async function listen() {
-      const clickEvents: Array<PointerEvent> = [];
-      const ac = new AbortController();
-      for await (const event of on.click(document, { signal: ac.signal })) {
-        clickEvents.push(event);
-        if (clickEvents.length === 3) {
-          ac.abort("Reached limit of 3 clicks");
-        }
-      }
-      return clickEvents;
-    }
+    const ac = new AbortController();
 
-    const listenPromise = listen();
+    const listenPromise = Array.fromAsync(
+      take(on.click(document, { signal: ac.signal }), 3),
+    );
 
     await clickThrice();
+
+    ac.abort();
+
+    await body.click();
 
     await expect(listenPromise).resolves.toHaveLength(3);
   });
@@ -71,6 +65,7 @@ describe("on", () => {
       });
 
       ac.abort("Oops!");
+
       await expect(iter.next()).resolves.toEqual({
         done: true,
         value: "Oops!",
